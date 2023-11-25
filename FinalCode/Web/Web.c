e12fb32f772877ac97b4916315335afdc4e9c523
LilZBJ <114568640+LilZBJ@users.noreply.github.com>
2023-11-25 23:55:42 +0800

    Update Web.c

--- FinalCode/Web/Web.c
+++ FinalCode/Web/Web.c
@@ -9,24 +9,29 @@ void vHttpServerTask(void *pvParameters) {
 
     // Connect to Wi-Fi
     cyw43_arch_init();
+    // Enable station mode (connecting to Wi-Fi as a client)
     cyw43_arch_enable_sta_mode();
 
     while(cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000) != 0) {
         printf("Attempting to connect...\n");
     }
 
+    // Connection successful message
     printf("Connected!\n");
 
     // Print network information
     //ip4_addr_t ipaddr, netmask, gw;
     struct netif *netif = netif_default;
+    // Get the default network interface
     if (netif) {
         printf("IP Address: %s\n", ip4addr_ntoa(&netif->ip_addr));
         printf("Netmask: %s\n", ip4addr_ntoa(&netif->netmask));
         printf("Gateway: %s\n", ip4addr_ntoa(&netif->gw));
-    } else {
+    //if network interface can't be found
+    }//if 
+    else {
         printf("Network interface not found\n");
-    }
+    }//else
 
     // Initialize web server
 
@@ -36,6 +41,5 @@ void vHttpServerTask(void *pvParameters) {
         // Add any additional HTTP server-related functionality or handling here
         vTaskDelay(pdMS_TO_TICKS(1000));  // Delay for 1 second
 
-    }
-}
-
+    }//while
+}//task
